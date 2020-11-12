fc58f2735dd03de006b4aa3b8209b4a6c77fb9a6
FranciscoJoseCaroCarazo <fcarocarazo@safareyes.es>
2020-11-12 09:25:55 +0100

    Terminado

--- index.ts
+++ index.ts
@@ -1,19 +1,23 @@
 // Tipad
 // Para determinar el tipo de los parámetros de la función, debes dar por hecho que los tipos van a ser siempre los de las dos variables que se añaden en la llamada de la linea 20.
 
+//Tipamos las Variables
 let a: number;
 let b: string;
 let c: boolean | number;
 
+//Se asignan valores a las variables
 a = 5;
 b = "texto";
 c = true;
 
+//If que sobreescribe el valor de la variable c
 if (Math.random() >= 0.5) {
   c = 100;
 }
 
-function d(param1: number, param2: number | boolean):number|string|boolean {
+//funcion que devuelve si param2 es del mismo tipo
+function d(param1: number, param2: number | boolean):number|string {
   if (typeof param2 === "number") {
     return param1 + param2;
   } else {
@@ -21,4 +25,5 @@ function d(param1: number, param2: number | boolean):number|string|boolean {
   }
 }
 
+//Imprimimos por consola el resultado
 console.log(d(a, c));
